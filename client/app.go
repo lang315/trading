ee6c5c9ab20e38685419df217747dcf7f7ff1f32
lang315 <langtucodon315@gmail.com>
2018-06-08 16:41:48 +0700

    Update index.html

--- client/app.go
+++ client/app.go
@@ -2,11 +2,14 @@ package main
 
 import "github.com/gopherjs/jquery"
 
-func main()  {
+func main() {
 	jquery.NewJQuery(".navbar-burger").On(jquery.CLICK, func(e jquery.Event) {
 		jquery.NewJQuery(e.CurrentTarget).ToggleClass("is-active")
 		jquery.NewJQuery(".navbar-menu").ToggleClass("is-active")
 	})
 
+	jquery.NewJQuery("#login").On(jquery.CLICK, func(e jquery.Event) {
+		jquery.NewJQuery("modal").Show()
+	})
 
 }
